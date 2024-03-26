313c68766060df8f37117571a6f164a496424259
SosGutnov <soslan.gutnov157@gmail.com>
2024-03-26 22:25:44 +0300

    Добавьте файлы проекта.

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxP = new System.Windows.Forms.TextBox();
+            this.textBoxQ = new System.Windows.Forms.TextBox();
+            this.textBoxSentence = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonEncrypted = new System.Windows.Forms.Button();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonUncrypted = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxUncrypted = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Q";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(310, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Слово";
+            // 
+            // textBoxP
+            // 
+            this.textBoxP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxP.Location = new System.Drawing.Point(48, 67);
+            this.textBoxP.Name = "textBoxP";
+            this.textBoxP.Size = new System.Drawing.Size(100, 22);
+            this.textBoxP.TabIndex = 3;
+            // 
+            // textBoxQ
+            // 
+            this.textBoxQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxQ.Location = new System.Drawing.Point(48, 104);
+            this.textBoxQ.Name = "textBoxQ";
+            this.textBoxQ.Size = new System.Drawing.Size(100, 22);
+            this.textBoxQ.TabIndex = 4;
+            // 
+            // textBoxSentence
+            // 
+            this.textBoxSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSentence.Location = new System.Drawing.Point(378, 69);
+            this.textBoxSentence.Name = "textBoxSentence";
+            this.textBoxSentence.Size = new System.Drawing.Size(368, 22);
+            this.textBoxSentence.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(94, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(536, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(536, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "3";
+            // 
+            // buttonEncrypted
+            // 
+            this.buttonEncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEncrypted.Location = new System.Drawing.Point(436, 185);
+            this.buttonEncrypted.Name = "buttonEncrypted";
+            this.buttonEncrypted.Size = new System.Drawing.Size(212, 39);
+            this.buttonEncrypted.TabIndex = 9;
+            this.buttonEncrypted.Text = "Зашифровать";
+            this.buttonEncrypted.UseVisualStyleBackColor = true;
+            this.buttonEncrypted.Click += new System.EventHandler(this.buttonEncrypted_Click);
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxEncrypted.Location = new System.Drawing.Point(378, 258);
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(368, 22);
+            this.textBoxEncrypted.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(123, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(249, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Зашифрованное сообщение";
+            // 
+            // buttonUncrypted
+            // 
+            this.buttonUncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUncrypted.Location = new System.Drawing.Point(436, 358);
+            this.buttonUncrypted.Name = "buttonUncrypted";
+            this.buttonUncrypted.Size = new System.Drawing.Size(212, 39);
+            this.buttonUncrypted.TabIndex = 12;
+            this.buttonUncrypted.Text = "Расшифровать";
+            this.buttonUncrypted.UseVisualStyleBackColor = true;
+            this.buttonUncrypted.Click += new System.EventHandler(this.buttonUncrypted_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(115, 440);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(257, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Расшифрованное сообщение";
+            // 
+            // textBoxUncrypted
+            // 
+            this.textBoxUncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUncrypted.Location = new System.Drawing.Point(378, 440);
+            this.textBoxUncrypted.Name = "textBoxUncrypted";
+            this.textBoxUncrypted.Size = new System.Drawing.Size(368, 22);
+            this.textBoxUncrypted.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(536, 318);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(19, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 497);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxUncrypted);
+            this.Controls.Add(this.buttonUncrypted);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.buttonEncrypted);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSentence);
+            this.Controls.Add(this.textBoxQ);
+            this.Controls.Add(this.textBoxP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxP;
+        private System.Windows.Forms.TextBox textBoxQ;
+        private System.Windows.Forms.TextBox textBoxSentence;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonEncrypted;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonUncrypted;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxUncrypted;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
